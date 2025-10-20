06c5e6da8537803bd91785710d7f3bec9d850672
Frederik Marx <frederik.marx@mailbox.org>
2025-10-20 06:35:16 +0200

    added respnsiveness to sidebar;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,43 +25,46 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { LogInIcon } from "lucide-react";
 import Link from "next/link";
 import { SidebarUserButton } from "./features/users/components/SidebarUserButton";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 export default function Home() {
   return (
     <>
       <SidebarProvider>
-        <Sidebar collapsible="icon" className="overflow-hidden">
-          <SidebarHeader className="flex-row">
-            <SidebarTrigger />
-            <span className="text-xl">Netzschmiede</span>
-          </SidebarHeader>
-          <SidebarContent>
-            <SidebarGroup>
-              <SidebarMenu>
-                <SignedOut>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <Link href="/sign-in">
-                        <LogInIcon />
-                        <span>Log in</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SignedOut>
-              </SidebarMenu>
-            </SidebarGroup>
-          </SidebarContent>
-          <SidebarFooter>
-            <SidebarGroup>
-              <SidebarMenu>
-                <SignedIn>
-                  <SidebarUserButton />
-                </SignedIn>
-              </SidebarMenu>
-            </SidebarGroup>
-          </SidebarFooter>
-        </Sidebar>
-        <main className="flex-1"></main>
+        <AppSidebarClient>
+          <Sidebar collapsible="icon" className="overflow-hidden">
+            <SidebarHeader className="flex-row">
+              <SidebarTrigger />
+              <span className="text-xl">Netzschmiede</span>
+            </SidebarHeader>
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarMenu>
+                  <SignedOut>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton asChild>
+                        <Link href="/sign-in">
+                          <LogInIcon />
+                          <span>Log in</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </SignedOut>
+                </SidebarMenu>
+              </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+              <SidebarGroup>
+                <SidebarMenu>
+                  <SignedIn>
+                    <SidebarUserButton />
+                  </SignedIn>
+                </SidebarMenu>
+              </SidebarGroup>
+            </SidebarFooter>
+          </Sidebar>
+          <main className="flex-1"></main>
+        </AppSidebarClient>
       </SidebarProvider>
     </>
   );
