5f4b583a1e3fa2c5cb1cf68fb97f3aff72e95361
Frederik Marx <frederik.marx@mailbox.org>
2025-10-24 03:13:52 +0200

    modified Organization schema

--- src/drizzle/schema/organizations.ts
+++ src/drizzle/schema/organizations.ts
@@ -4,7 +4,6 @@ import {pgTable, varchar} from "drizzle-orm/pg-core";
 export const OrganizationTable = pgTable("organizations", {
     id: varchar().primaryKey(),
     name: varchar().notNull(),
-    email: varchar().notNull(),
     imageUrl: varchar().notNull(),
     createdAt: timestamp().notNull().defaultNow(),
     updatedAt: timestamp().notNull().defaultNow(),
