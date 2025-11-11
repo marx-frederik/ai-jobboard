29727552d7c51711cfb60e47b6adcd681b4e9a2b
Frederik Marx <frederik.marx@mailbox.org>
2025-11-11 07:27:08 +0100

    started employer dashboard page;

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -1,3 +1,26 @@
-export default function EmployerHomePage(){
-    return <h1>Hello Employer</h1>
+import { db } from "@/drizzle/db";
+import { JobListingTable } from "@/drizzle/schema";
+import getCurrentOrganization from "@/services/clerk/lib/getCurrentOrganization";
+import { desc, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+
+export default async function EmployerHomePage() {
+  const { orgId } = await getCurrentOrganization();
+  if (orgId == null) return null;
+
+  const jobListing = await getMostRecentJobListing(orgId)
+  if (jobListing == null){
+    redirect("/employer/job-listings/new")
+  }else{
+    redirect(`emplyoer/job-listings/${jobListing.id}`)
+  }
+}
+
+async function getMostRecentJobListing(orgId:string){
+    return db.query.JobListingTable.findFirst(
+        {
+            where: eq(JobListingTable.organizationId, orgId),
+            orderBy: desc(JobListingTable.createdAt)
+        }
+    )
 }
