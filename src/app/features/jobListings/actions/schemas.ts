caac719b01321efb48988325695a5aaef72f3860
Frederik Marx <frederik.marx@mailbox.org>
2025-11-13 14:50:34 +0100

    added schema

---
+++ src/app/features/jobListings/actions/schemas.ts
@@ -0,0 +1,44 @@
+import {
+  experienceLevels,
+  jobListingTypes,
+  locationRequirements,
+  wageIntervals,
+} from "@/drizzle/schema";
+import z from "zod";
+
+export const jobListingSchema = z
+  .object({
+    title: z.string().min(1, "Required"),
+    description: z.string().min(1, "Required"),
+    experienceLevel: z.enum(experienceLevels),
+    locationRequirement: z.enum(locationRequirements),
+    type: z.enum(jobListingTypes),
+    wage: z.number().int().positive().min(1,"Must be positive").nullable(),
+    wageInterval: z.enum(wageIntervals),
+    stateAbbreviation: z
+      .string()
+      .transform((val) => (val.trim() === "" ? null : val))
+      .nullable(),
+    city: z
+      .string()
+      .transform((val) => (val.trim() === "" ? null : val))
+      .nullable(),
+  })
+  .refine(
+    (data) => {
+      return data.locationRequirement === "remote" || data.city != null;
+    },
+    {
+      message: "Required for non-remote listings",
+      path: ["city"],
+    }
+  )
+  .refine(
+    (data) => {
+      return data.locationRequirement === "remote" || data.stateAbbreviation != null;
+    },
+    {
+      message: "Required for non-remote listings",
+      path: ["stateAbbreviation"],
+    }
+  );
