708e00488b5989df78263d6b578e8c980e175a46
Frederik Marx <frederik.marx@mailbox.org>
2025-11-20 02:52:17 +0100

    added db update action function

--- src/app/features/jobListings/actions/actions.ts
+++ src/app/features/jobListings/actions/actions.ts
@@ -2,8 +2,11 @@
 import z from "zod";
 import { jobListingSchema } from "./schemas";
 import { auth } from "@clerk/nextjs/server";
-import { insertJobListing } from "../db/jobListing";
+import { insertJobListing, updateJobListing as updateJobListingDb } from "../db/jobListing";
 import { redirect } from "next/navigation";
+import { db } from "@/drizzle/db";
+import { eq, and } from "drizzle-orm";
+import { JobListingTable } from "@/drizzle/schema";
 
 export async function createJobListing(
   unsafeData: z.infer<typeof jobListingSchema>
@@ -20,7 +23,48 @@ export async function createJobListing(
       error: true,
       message: "There is an error ccreating your job listing",
     };
-  const jobListing = await insertJobListing({ organizationId:orgId, ...data, status: "draft" });
+  const jobListing = await insertJobListing({
+    organizationId: orgId,
+    ...data,
+    status: "draft",
+  });
 
-  redirect(`/employer/job-listings/${jobListing.id}`)
+  redirect(`/employer/job-listings/${jobListing.id}`);
+}
+
+export async function updateJobListing(
+  id: string,
+  unsafeData: z.infer<typeof jobListingSchema>
+) {
+  const { orgId } = await auth();
+  if (orgId == null)
+    return {
+      error: true,
+      message: "You don't have permission to create a job listing",
+    };
+  const { success, data } = jobListingSchema.safeParse(unsafeData);
+  if (!success)
+    return {
+      error: true,
+      message: "There was an error creating your job listing",
+    };
+
+  const jobListing = await getJobListing(id, orgId);
+  if(jobListing == null)return {
+    error: true,
+    message: "There was an error updating your job listing",
+  };
+
+  const updatedJobListing = await updateJobListingDb(id, data);
+
+  redirect(`/employer/job-listings/${updatedJobListing.id}`);
+}
+
+export async function getJobListing(jobListingId: string, organizationId: string) {
+  return db.query.JobListingTable.findFirst({
+    where: and(
+      eq(JobListingTable.id, jobListingId),
+      eq(JobListingTable.organizationId, organizationId)
+    ),
+  });
 }
