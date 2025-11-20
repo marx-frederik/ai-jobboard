57dab25b6f86603eb9741ab7e764f97ed0772d80
Frederik Marx <frederik.marx@mailbox.org>
2025-11-20 02:52:05 +0100

    added db update function

--- src/app/features/jobListings/db/jobListing.ts
+++ src/app/features/jobListings/db/jobListing.ts
@@ -1,11 +1,28 @@
 import { db } from "@/drizzle/db";
 import { JobListingTable } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+
 export async function insertJobListing(
   data: typeof JobListingTable.$inferInsert
 ) {
-  const [result] =  await db.insert(JobListingTable).values(data).returning({
+  const [result] = await db.insert(JobListingTable).values(data).returning({
     id: JobListingTable.id,
     organizationId: JobListingTable.organizationId,
   });
-  return result
+  return result;
+}
+
+export async function updateJobListing(
+  id: string,
+  jobListing: Partial<typeof JobListingTable.$inferInsert>
+) {
+  const [updatedListing] = await db
+    .update(JobListingTable)
+    .set(jobListing)
+    .where(eq(JobListingTable.id, id))
+    .returning({
+      id: JobListingTable.id,
+      organizationId: JobListingTable.organizationId,
+    });
+  return updatedListing;
 }
