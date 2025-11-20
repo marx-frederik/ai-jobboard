84d336899d4483baffbf73ba1ee94db4de01dfa7
Frederik Marx <frederik.marx@mailbox.org>
2025-11-20 01:57:45 +0100

    updated markdownpartial not to be default component

---
+++ src/components/markdown/MarkdownPartial.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { ReactNode, useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+
+export function MarkdownPartial({
+  mainMarkdown,
+  dialogMarkdown,
+  dialogTitle,
+}: {
+  mainMarkdown: ReactNode;
+  dialogMarkdown: ReactNode;
+  dialogTitle: string;
+}) {
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  const markdownRef = useRef<HTMLDivElement>(null);
+
+  function checkOverflow(node: HTMLDivElement) {
+    setIsOverflowing(node.scrollHeight > node.clientHeight);
+    /* "If the scrollcontainer size is larger than the client container size" */
+  }
+
+  useEffect(() => {
+    /* "We use the useEffect to react to window size changes to adjust the max-length of the text"*/
+    const controller = new AbortController();
+    window.addEventListener(
+      "resize",
+      () => {
+        if (markdownRef.current == null) return;
+        checkOverflow(markdownRef.current);
+      },
+      { signal: controller.signal }
+    );
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    /*This Hooks runs when all content on the page is rendered*/
+    if (markdownRef.current == null)
+      return; /* Wenn die markdownRef null ist, mache nichts */
+    checkOverflow(markdownRef.current);
+  });
+
+  return (
+    <>
+      <div ref={markdownRef} className="max-h-[300px] overflow-hidden relative">
+        {mainMarkdown}
+        {isOverflowing && (
+          <div className="bg-gradient-to-t from-background to-transparent to-15% inset-0 absolute pointer-events-none" />
+        )}
+      </div>
+      {isOverflowing && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost" className="underline -ml-3">
+              Read More
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="md:max-w-3xl lg:max-w-4xl max-h-[calc(100%-2rem)] overflow-hidden flex flex-col">
+            <DialogHeader>
+              <DialogTitle>{dialogTitle}</DialogTitle>
+            </DialogHeader>
+            <div className="flex-1 overflow-y-auto">{dialogMarkdown}</div>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
+  );
+}
