d3fef14b2cef44f3f3615f07066cab9f52e95d16
Frederik Marx <frederik.marx@mailbox.org>
2025-11-21 04:36:27 +0100

    added permission check for creating and updating job listings

--- src/app/features/jobListings/actions/actions.ts
+++ src/app/features/jobListings/actions/actions.ts
@@ -2,17 +2,24 @@
 import z from "zod";
 import { jobListingSchema } from "./schemas";
 import { auth } from "@clerk/nextjs/server";
-import { insertJobListing, updateJobListing as updateJobListingDb } from "../db/jobListing";
+import {
+  insertJobListing,
+  updateJobListing as updateJobListingDb,
+} from "../db/jobListing";
 import { redirect } from "next/navigation";
 import { db } from "@/drizzle/db";
 import { eq, and } from "drizzle-orm";
 import { JobListingTable } from "@/drizzle/schema";
+import { hasOrgUserPermission } from "@/services/clerk/lib/orgUserPermissions";
 
 export async function createJobListing(
   unsafeData: z.infer<typeof jobListingSchema>
 ) {
   const { orgId } = await auth();
-  if (orgId == null)
+  if (
+    orgId == null ||
+    !(await hasOrgUserPermission("org:job_listing:job_listing_create"))
+  )
     return {
       error: true,
       message: "You don't have permission to create a job listing",
@@ -37,7 +44,10 @@ export async function updateJobListing(
   unsafeData: z.infer<typeof jobListingSchema>
 ) {
   const { orgId } = await auth();
-  if (orgId == null)
+  if (
+    orgId == null ||
+    !(await hasOrgUserPermission("org:job_listing:job_listing_update"))
+  )
     return {
       error: true,
       message: "You don't have permission to create a job listing",
@@ -50,17 +60,21 @@ export async function updateJobListing(
     };
 
   const jobListing = await getJobListing(id, orgId);
-  if(jobListing == null)return {
-    error: true,
-    message: "There was an error updating your job listing",
-  };
+  if (jobListing == null)
+    return {
+      error: true,
+      message: "There was an error updating your job listing",
+    };
 
   const updatedJobListing = await updateJobListingDb(id, data);
 
   redirect(`/employer/job-listings/${updatedJobListing.id}`);
 }
 
-export async function getJobListing(jobListingId: string, organizationId: string) {
+export async function getJobListing(
+  jobListingId: string,
+  organizationId: string
+) {
   return db.query.JobListingTable.findFirst({
     where: and(
       eq(JobListingTable.id, jobListingId),
