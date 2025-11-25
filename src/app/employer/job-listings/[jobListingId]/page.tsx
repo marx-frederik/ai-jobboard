08e2c99e81bf926ef47f1b1f66694407806288f7
Frederik Marx <frederik.marx@mailbox.org>
2025-11-25 09:23:45 +0100

    added permission check in joblisting page

--- src/app/employer/job-listings/[jobListingId]/page.tsx
+++ src/app/employer/job-listings/[jobListingId]/page.tsx
@@ -1,4 +1,4 @@
-import { JobListingTable } from "@/drizzle/schema";
+import { JobListingStatus, JobListingTable } from "@/drizzle/schema";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/drizzle/db";
 import { eq, and } from "drizzle-orm";
@@ -8,9 +8,15 @@ import { formatJobListingStatus } from "@/app/features/jobListings/lib/formatter
 import JobListingBadges from "@/app/features/jobListings/components/jobListingBadges";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { EditIcon } from "lucide-react";
+import { EditIcon, EyeIcon, EyeOffIcon } from "lucide-react";
 import MarkdownRenderer from "@/components/markdown/MarkdownRenderer";
-import {MarkdownPartial} from "@/components/markdown/MarkdownPartial";
+import { MarkdownPartial } from "@/components/markdown/MarkdownPartial";
+import AsyncIf from "@/components/AsyncIf";
+import { hasOrgUserPermission } from "@/services/clerk/lib/orgUserPermissions";
+import { getNextJobListingStatus } from "@/app/features/jobListings/lib/utils";
+import { hasPlanFeature } from "@/services/clerk/lib/planFeatures";
+import { hasReachedMaxFeaturedJobListings } from "@/app/features/jobListings/lib/planFeatureHelpers";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 
 export default async function JobListingPage({
   params,
@@ -39,12 +45,19 @@ export default async function JobListingPage({
       </div>
       <div className="flex items-center gap-2 empty:-mt-4">
         {/* -mt-4 um bei leerem div die gap-4 von oben zu kontern */}
-        <Button asChild variant="outline">
-          <Link href={`/employer/job-listings/${jobListing.id}/edit`}>
-            <EditIcon className="size-4" />
-            Edit
-          </Link>
-        </Button>
+        <AsyncIf
+          condition={() =>
+            hasOrgUserPermission("org:job_listing:job_listing_update")
+          }
+        >
+          <Button asChild variant="outline">
+            <Link href={`/employer/job-listings/${jobListing.id}/edit`}>
+              <EditIcon className="size-4" />
+              Edit
+            </Link>
+          </Button>
+        </AsyncIf>
+        <StatusUpdateButton status={jobListing.status} />
       </div>
       <div></div>
       <MarkdownPartial
@@ -59,6 +72,72 @@ export default async function JobListingPage({
       />
     </div>
   );
+}
+
+function StatusUpdateButton({ status }: { status: JobListingStatus }) {
+  const button = (
+    <Button variant="outline">
+      <EditIcon className="size-4" />
+      Toggle
+    </Button>
+  );
+  return (
+    <AsyncIf
+      condition={() =>
+        hasOrgUserPermission("org:job_listing:job_listing_change_status")
+      }
+    >
+      {getNextJobListingStatus(status) === "published" ? (
+        <AsyncIf
+          condition={async () => {
+            const isMaxed = await hasReachedMaxFeaturedJobListings();
+            return !isMaxed;
+          }}
+          otherwise={
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline">
+                  {statusToggleButtonText(status)}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="flex flex-col gap-2">
+                You must upgrade your plan to publish more job listings.
+                <Button asChild>
+                  <Link href="/employer/pricing">Upgrade Plan</Link>
+                </Button>
+              </PopoverContent>
+            </Popover>
+          }
+        >
+          {button}
+        </AsyncIf>
+      ) : (
+        button
+      )}
+    </AsyncIf>
+  );
+}
+
+function statusToggleButtonText(status: JobListingStatus) {
+  switch (status) {
+    case "delisted":
+    case "draft":
+      return (
+        <>
+          <EyeIcon className="size-4" />
+          Publish
+        </>
+      );
+    case "published":
+      return (
+        <>
+          <EyeOffIcon className="size-4" />
+          Delist
+        </>
+      );
+    default:
+      throw new Error(`Unknown status ${status satisfies never}`);
+  }
 }
 
 export function getJobListing(jobListingId: string, organizationId: string) {
