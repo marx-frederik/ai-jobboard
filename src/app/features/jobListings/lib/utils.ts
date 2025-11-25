c2b668642e83522ea392f75e058874002b388f9f
Frederik Marx <frederik.marx@mailbox.org>
2025-11-25 09:23:08 +0100

    added function to get next possible status of job listing

---
+++ src/app/features/jobListings/lib/utils.ts
@@ -0,0 +1,10 @@
+import { JobListingStatus } from "@/drizzle/schema";
+
+export function getNextJobListingStatus(status:JobListingStatus){
+    switch(status){
+        case "draft":
+        case "delisted":return "published" 
+        case "published":return "delisted"
+        default: throw new Error("Unknown Job Listing status")
+    }
+}
