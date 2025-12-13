8d71a6e5803a7b9e1512dffca3702780c3551395
Frederik Marx <frederik.marx@mailbox.org>
2025-12-13 03:10:30 +0100

    added JobListingItems component

--- src/app/(job-seeker)/page.tsx
+++ src/app/(job-seeker)/page.tsx
@@ -1,3 +1,13 @@
-export default function HomePage() {
-  return <>Hello World </>;
+import { JobListingItems } from "./_shared/JobListingItems";
+
+export default function HomePage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | string[]>>;
+}) {
+  return (
+    <div className="m-4">
+      <JobListingItems searchParams={searchParams} />
+    </div>
+  );
 }
