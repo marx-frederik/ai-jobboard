58e0e782a07002984f0f7c9bfb49a2f037b3efad
Frederik Marx <frederik.marx@mailbox.org>
2025-12-15 00:52:22 +0100

    created jobboard sidebar component that will host the filter form

---
+++ src/app/(job-seeker)/_shared/JobBoardSidebar.tsx
@@ -0,0 +1,12 @@
+import { JobListingFilterForm } from "@/app/features/jobListings/components/JobListingFilterForm";
+import { SidebarGroup, SidebarGroupContent } from "@/components/ui/sidebar";
+
+export function JobBoardSidebar() {
+  return (
+    <SidebarGroup className="group-data-[state=collapsed]:hidden">
+      <SidebarGroupContent className="px-1">
+        <JobListingFilterForm />
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
