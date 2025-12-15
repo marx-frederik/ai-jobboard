5a9ada45e8a4b2f3f7e0007ee9c7dbc53049f666
Frederik Marx <frederik.marx@mailbox.org>
2025-12-15 08:23:03 +0100

    renamed exported markdown editor

--- src/components/markdown/MarkdownEditor.tsx
+++ src/components/markdown/MarkdownEditor.tsx
@@ -12,7 +12,7 @@ const Editor = dynamic(() => import('./InitializedMDXEditor'), {
 
 // This is what is imported by other components. Pre-initialized with plugins, and ready
 // to accept other props, including a ref.
-export const ForwardRefEditor = forwardRef<MDXEditorMethods, MDXEditorProps>((props, ref) => <Editor {...props} editorRef={ref} />)
+export const MarkdownEditor = forwardRef<MDXEditorMethods, MDXEditorProps>((props, ref) => <Editor {...props} editorRef={ref} />)
 
 // TS complains without the following line
-ForwardRefEditor.displayName = 'ForwardRefEditor'
+MarkdownEditor.displayName = 'ForwardRefEditor'
