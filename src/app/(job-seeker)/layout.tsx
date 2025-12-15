4a84e9c465c6c58c0b003ff05c829cebf76ee59f
Frederik Marx <frederik.marx@mailbox.org>
2025-12-15 01:00:57 +0100

    embedded parallel route into layout componente

--- src/app/(job-seeker)/layout.tsx
+++ src/app/(job-seeker)/layout.tsx
@@ -1,14 +1,6 @@
 import { ReactNode } from "react";
-import {
-  SidebarGroup,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/AppSidebar";
 import { SidebarUserButton } from "../features/users/components/SidebarUserButton";
-import { SignedOut } from "@clerk/nextjs";
-import Link from "next/link";
 import {
   BrainCircuitIcon,
   Brain,
@@ -18,33 +10,42 @@ import {
 } from "lucide-react";
 import SidebarNavMenuGroup from "@/components/sidebar/SidebarNavMenuGroup";
 
-export default function JobSeekerLayout({ children }: { children: ReactNode }) {
+export default function JobSeekerLayout({
+  children,
+  sidebar,
+}: {
+  children: ReactNode;
+  sidebar: ReactNode;
+}) {
   return (
     <AppSidebar
       content={
-        <SidebarNavMenuGroup
-          className="mt-auto"
-          items={[
-            { href: "/", icon: <ClipboardListIcon />, label: "Jobboard" },
-            {
-              href: "/ai-search",
-              icon: <BrainCircuitIcon />,
-              label: "AI Search",
-            },
-            {
-              href: "/employer",
-              icon: <LayoutDashboardIcon />,
-              label: "Employer Dashboard",
-              authStatus:"signedIn"
-            },
-            {
-              href: "/sign-in",
-              icon: <LogInIcon />,
-              label: "Sign In",
-              authStatus:"signedOut"
-            },
-          ]}
-        />
+        <>
+          {sidebar}
+          <SidebarNavMenuGroup
+            className="mt-auto"
+            items={[
+              { href: "/", icon: <ClipboardListIcon />, label: "Jobboard" },
+              {
+                href: "/ai-search",
+                icon: <BrainCircuitIcon />,
+                label: "AI Search",
+              },
+              {
+                href: "/employer",
+                icon: <LayoutDashboardIcon />,
+                label: "Employer Dashboard",
+                authStatus: "signedIn",
+              },
+              {
+                href: "/sign-in",
+                icon: <LogInIcon />,
+                label: "Sign In",
+                authStatus: "signedOut",
+              },
+            ]}
+          />
+        </>
       }
       footerButton={<SidebarUserButton />}
     >
