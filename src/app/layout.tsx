bea9945eb8ca3867821d334ff50a6c38babfe703
Frederik Marx <frederik.marx@mailbox.org>
2025-12-30 08:56:45 +0100

    added SSR optimization for UploadThing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "@mdxeditor/editor/style.css"
+import "@mdxeditor/editor/style.css";
 import ClerkProvider from "@/services/clerk/components/ClerkProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { customFileRouter } from "@/services/uploadthing/router";
+import { extractRouterConfig } from "uploadthing/server";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +34,17 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(customFileRouter)}
+          />
           {children}
-          <Toaster/>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
