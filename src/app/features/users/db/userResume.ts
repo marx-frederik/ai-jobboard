2062970de11a9ec78a489260d771f98ba93c47bd
Frederik Marx <frederik.marx@mailbox.org>
2025-12-30 08:58:23 +0100

    added database resume upload

---
+++ src/app/features/users/db/userResume.ts
@@ -0,0 +1,27 @@
+import { db } from "@/drizzle/db";
+import { UserResumeTable } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+
+export async function upsertUserResume(
+  userId: string,
+  data: Omit<typeof UserResumeTable.$inferInsert, "userId">
+) {
+  await db
+    .insert(UserResumeTable)
+    .values({ userId, ...data })
+    .onConflictDoUpdate({
+      target: UserResumeTable.userId,
+      set: data,
+    });
+  //TODO:Caching
+}
+
+export async function updateUserResume(
+  userId: string,
+  data: Partial<Omit<typeof UserResumeTable.$inferInsert, "userId">>
+) {
+  await db
+    .update(UserResumeTable)
+    .set(data)
+    .where(eq(UserResumeTable.userId, userId));
+}
